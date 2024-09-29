297dd25e6b649fa9758adc46f9a9d42bbed80974
Hisham Montaser <hisham.montaser@outlook.com>
2024-09-29 23:03:37 +0300

    new

--- Backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "My-RG"
-    storage_account_name = "bastawisiterraformsa"
-    container_name = "mytfstate"
-    key = "terraform.tfstate"
-  }
-}
